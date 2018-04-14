865f3909ca468de502e0b842144f90b8307905b9
Neeraj Kumar Sharma <37273063+DevrajS@users.noreply.github.com>
2018-04-14 20:17:13 +0530

    compare triplets

---
+++ Compare the triplets/Solution.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2) {
+        
+        int score_a;
+        int score_b;
+        score_a=score_b=0;
+        if(a0>b0)
+        {
+            score_a++;    
+        }
+        else if(a0<b0)
+        {
+            score_b++;
+        }
+        if(a1>b1)
+        {
+            score_a++;
+        }
+        else if(a1<b1){
+            score_b++;
+        }
+        if(a2>b2)
+        {
+            score_a++;
+        }
+        else if(a2<b2)
+        {
+            score_b++;
+        }
+   
+        int [] arr=new int [2];
+        arr[0]=score_a;
+        arr[1]=score_b;
+        
+        return arr;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] a0A1A2 = scan.nextLine().split(" ");
+
+        int a0 = Integer.parseInt(a0A1A2[0].trim());
+
+        int a1 = Integer.parseInt(a0A1A2[1].trim());
+
+        int a2 = Integer.parseInt(a0A1A2[2].trim());
+
+        String[] b0B1B2 = scan.nextLine().split(" ");
+
+        int b0 = Integer.parseInt(b0B1B2[0].trim());
+
+        int b1 = Integer.parseInt(b0B1B2[1].trim());
+
+        int b2 = Integer.parseInt(b0B1B2[2].trim());
+
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+
+        for (int resultItr = 0; resultItr < result.length; resultItr++) {
+            bw.write(String.valueOf(result[resultItr]));
+
+            if (resultItr != result.length - 1) {
+                bw.write(" ");
+            }
+        }
+
+        bw.newLine();
+
+        bw.close();
+    }
+}
+
+
+// Author Neeraj sharma @ computer sector.
---
+++ Compare the triplets/readme_first.txt
@@ -0,0 +1,11 @@
+if-else based problem
+# this will judge your decision making skill.
+
+Follow are the link of the problem.
+
+
+
+https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+
+
